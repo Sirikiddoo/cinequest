a9ca90fb21a808b38557faf90e4a1f3e2ae13c6d
sirikiddoo <siri.bastings@gmail.com>
2024-08-04 19:20:06 -0300

    log-sign-in-css

--- src/pages/signin/SignIn.jsx
+++ src/pages/signin/SignIn.jsx
@@ -1,15 +1,37 @@
 import './SignIn.css'
+import emailIcon from '../../assets/images/envelope.png'
+import passwordIcon from '../../assets/images/lock.png'
+import React from "react";
+import {Link, NavLink} from "react-router-dom";
+
+
 
 function SignIn() {
     return (
-        <div>
-            <section>
-                <h2>LOG IN</h2>
-                <form>
-                    <input type="text" placeholder="username"></input>
-                    <input type="password" placeholder="password"></input>
-                    <button>Log In</button>
+        <div className="sign-in">
+            <section className="sign-in-container">
+                <div className="container-text">
+                    <h2 className="text-item-left">LOG IN</h2>
+                    <h2 className="text-item-right">SIGN UP</h2>
+                </div>
+                <form className="sign-in-form">
+                    <div className="input-container">
+                        <img src={emailIcon} alt="Email Icon" className="input-icon"></img>
+                        <input type="email" placeholder="Email" />
+                    </div>
+                    <div className="input-container">
+                        <img src={passwordIcon} alt="Password Icon" className="input-icon"></img>
+                        <input type="password" placeholder="Password" />
+                    </div>
+                    <button>Continue</button>
                 </form>
+                <div className="sign-in-text">
+                    <p className="text-left">New to CineQuest?</p>
+                    <navLink to="/signin">SIGN UP</navLink>
+                </div>
+                <div className="home-button-container">
+                    <Link to="/" className="home-button">Back to Homepage &#8594;</Link>
+                </div>
             </section>
         </div>
     );
--- src/pages/signup/SignUp.jsx
+++ src/pages/signup/SignUp.jsx
@@ -1,23 +1,41 @@
 import './SignUp.css'
+import userIcon from '../../assets/images/circle-user.png'
+import emailIcon from '../../assets/images/envelope.png'
+import passwordIcon from '../../assets/images/lock.png'
+import React from "react";
+import { Link } from "react-router-dom";
+
 
 
 function SignUp() {
     return (
-        <div>
+        <div className="sign-up">
             <section className="sign-up-container">
                 <div className="container-text">
                 <h2 className="text-item-left">SIGN UP</h2>
                 <h2 className="text-item-right">LOG IN</h2>
                 </div>
                 <form className="sign-up-form">
-                    <input type="text" placeholder="Username"></input>
-                    <input type="email" placeholder="Email"></input>
-                    <input type="password" placeholder="Password"></input>
+                    <div className="input-container">
+                        <img src={userIcon} alt="User Icon" className="input-icon"></img>
+                        <input type="text" placeholder="Username" />
+                    </div>
+                    <div className="input-container">
+                        <img src={emailIcon} alt="Email Icon" className="input-icon"></img>
+                        <input type="email" placeholder="Email" />
+                    </div>
+                    <div className="input-container">
+                        <img src={passwordIcon} alt="Password Icon" className="input-icon"></img>
+                        <input type="password" placeholder="Password" />
+                    </div>
                     <button>Join</button>
                 </form>
                 <div className="log-in-text">
                 <p className="text-left">Already a user?</p>
                 <navlink to="/signin">LOG IN</navlink>
+                </div>
+                <div className="home-button-container">
+                    <Link to="/" className="home-button">Back to Homepage &#8594;</Link>
                 </div>
             </section>
         </div>
